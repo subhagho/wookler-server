c6c8ccc3b610467770182874cc3064d0de114be6
Subho Ghosh <subhagho@arevue.com>
2012-07-16 19:41:06 +0530

    Fixed Launch issues.
    Added shell script to execute the server start.

---
+++ core/runserver.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+export BINDIR=./target/
+
+CLASSPATH=$CLASSPATH:.
+
+for JAR in `ls $BINDIR/*.jar`
+do
+	CLASSPATH=$CLASSPATH:$JAR
+done
+for JAR in `ls $BINDIR/lib/*.jar`
+do
+	CLASSPATH=$CLASSPATH:$JAR
+done
+export CLASSPATH
+echo "Using classpath $CLASSPATH..."
+java -cp $CLASSPATH $@
