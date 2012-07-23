5cddf3aae238c389ad8642baea2dfcf1d3e75fe3
Subho Ghosh <subhagho@arevue.com>
2012-07-23 19:42:18 +0530

    Added the service to retrieve tags.
    URL example :
    http://localhost:8090/rest/wookler/core/data/tags/{videoid}/{seqid or
    "-"}/q=<query on tags>&p=<page offset>&s=<page size>

--- core/src/main/java/com/wookler/services/WooklerCoreServices.java
+++ core/src/main/java/com/wookler/services/WooklerCoreServices.java
@@ -21,6 +21,7 @@ import com.wookler.core.persistence.AbstractEntity;
 import com.wookler.core.persistence.DataManager;
 import com.wookler.core.persistence.query.Query;
 import com.wookler.entities.Sequence;
+import com.wookler.entities.Tag;
 import com.wookler.entities.VideoMedia;
 import com.wookler.utils.LogUtils;
 
@@ -207,6 +208,77 @@ public class WooklerCoreServices {
 	 * @return
 	 * @throws Exception
 	 */
+	@Path("/tags/{videoid}/{seqid}")
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public JResponse<WooklerResponse> tags(
+			@DefaultValue(_EMPTY_PATH_ELEMENT_) @PathParam("videoid") String videoid,
+			@DefaultValue(_EMPTY_PATH_ELEMENT_) @PathParam("seqid") String seqid,
+			@DefaultValue("") @QueryParam("q") String query,
+			@DefaultValue("1") @QueryParam("p") String page,
+			@DefaultValue("20") @QueryParam("s") String size) throws Exception {
+		try {
+			log.debug("VIDEO-ID:" + videoid);
+			StringBuffer squery = new StringBuffer("MEDIAID=" + videoid);
+			if (seqid.compareTo(_EMPTY_PATH_ELEMENT_) != 0) {
+				squery.append(Query._QUERY_CONDITION_AND_).append("SEQID=")
+						.append(seqid);
+			}
+			if (query != null && !query.isEmpty()
+					&& query.compareTo(_EMPTY_PATH_ELEMENT_) != 0)
+				squery = squery.append(Query._QUERY_CONDITION_AND_).append(
+						query);
+			int pagec = Integer.parseInt(page);
+			int limit = Integer.parseInt(size);
+			int count = pagec * limit;
+			squery = squery.append(Query._QUERY_CONDITION_AND_)
+					.append("LIMIT ").append(count);
+
+			log.debug("QUERY [" + squery + "]");
+
+			DataManager dm = DataManager.get();
+			List<AbstractEntity> data = dm.read(squery.toString(), Tag.class);
+			WooklerResponse response = new WooklerResponse();
+			String path = "/videos/" + videoid + "?q=" + squery.toString();
+			response.setRequest(path);
+			if (data == null || data.size() <= 0) {
+				response.setState(EnumResponseState.NoData);
+			} else {
+				response.setState(EnumResponseState.Success);
+				int stindex = limit * (pagec - 1);
+				if (stindex > 0) {
+					if (stindex > data.size()) {
+						response.setState(EnumResponseState.NoData);
+					} else {
+						List<AbstractEntity> subdata = data.subList(stindex,
+								data.size());
+						response.setData(subdata);
+					}
+				} else
+					response.setData(data);
+			}
+			return JResponse.ok(response).build();
+		} catch (Exception e) {
+			LogUtils.stacktrace(log, e);
+			log.error(e.getLocalizedMessage());
+
+			WooklerResponse response = new WooklerResponse();
+			response.setState(EnumResponseState.Exception);
+			response.setMessage(e.getLocalizedMessage());
+			return JResponse.ok(response).build();
+		}
+	}
+
+	/**
+	 * Get the sequence associated with a Video Media.
+	 * 
+	 * @param videoid
+	 *            - Video ID (Required)
+	 * @param query
+	 *            - Filter Query (Optional), default '-'
+	 * @return
+	 * @throws Exception
+	 */
 	@Path("/sequences/{videoid}")
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
@@ -225,7 +297,8 @@ public class WooklerCoreServices {
 			int pagec = Integer.parseInt(page);
 			int limit = Integer.parseInt(size);
 			int count = pagec * limit;
-			squery = squery.append(";LIMIT ").append(count);
+			squery = squery.append(Query._QUERY_CONDITION_AND_)
+					.append("LIMIT ").append(count);
 
 			log.debug("QUERY [" + squery + "]");
 
